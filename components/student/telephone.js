6f291591d0c4699f54ef6a3031b7b6b5d3efb158
Chris Hopkins <80141050+hopkincm@users.noreply.github.com>
2023-03-10 15:04:30 -0500

    Adding Telephone Part 1

--- components/student/telephone.js
+++ components/student/telephone.js
@@ -1,3 +1,15 @@
 export default function TelephoneActivity() {
-  return <h2>Telephone</h2>
+  return <>
+    <h2>Telephone</h2>
+    <div className="container">
+      <div className="row">
+        <div class="col Melody"><button id="Melody" onclick="window.location.href='performMelody.html';">Melody</button></div>
+        <div class="col Baseline"><button id="Baseline" onclick="window.location.href='performBaseline.html';">Bassline</button></div>
+        <div class="col Creative"><button id="Creative" onclick="window.location.href='performCreative.html';">Creative</button></div>
+      </div>
+      <div className="row"><button id="Response" onclick="window.location.href='response.html';">Response</button></div>
+    </div>
+
+  </>
+
 };
